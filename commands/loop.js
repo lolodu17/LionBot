596b7ececa6eee74a56f1072c8e0ed3de2d3f3d5
IceeMC <johnburke915@gmail.com>
2017-12-03 19:08:31 -0800

    oof

--- commands/loop.js
+++ commands/loop.js
@@ -1,7 +1,7 @@
 module.exports.run = async(bot, message, args, func) => {
     const serverQueue = bot.queue.get(message.guild.id);
     if(!serverQueue || !message.member.voiceChannel) return message.channel.send("You must be in a voiceChannel. `CHANNEL_USER_ERR`. Or nothing is playing `NO_QUEUED_SONGS`");
-    if(bot.serverQueue.loop == false) {
+    if(serverQueue.loop == false) {
         serverQueue.loop == true;
         func.embed(message.channel, "I've enabled loopmode");
     } else {
